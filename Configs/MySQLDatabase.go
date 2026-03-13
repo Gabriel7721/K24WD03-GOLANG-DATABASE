0edf20726c7d86cb3d80f6ccb9d9910286547470
Gabriel7721 <116139790+Gabriel7721@users.noreply.github.com>
2026-03-13 16:19:21 +0700

    update database connect

--- Configs/MySQLDatabase.go
+++ Configs/MySQLDatabase.go
@@ -1,1 +1,30 @@
 package configs
+
+import (
+	"database/sql"
+	"fmt"
+	"os"
+
+	_ "github.com/go-sql-driver/mysql"
+	_ "github.com/joho/godotenv/autoload"
+)
+
+var MySQLDB *sql.DB
+
+func MySQLConnect() {
+	// "root:root@tcp(127.0.0.1:3306)/server02_golang_db?parseTime=true"
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?parseTime=true",
+		os.Getenv("MYSQL_USER"),
+		os.Getenv("MYSQL_PASSWORD"),
+		os.Getenv("MYSQL_HOST"),
+		os.Getenv("MYSQL_PORT"),
+		os.Getenv("MYSQL_DB"),
+	)
+
+	db, _ := sql.Open("mysql", dsn)
+	db.SetMaxOpenConns(10)
+	db.SetMaxIdleConns(5)
+
+	MySQLDB = db
+	fmt.Println("MySQL connected successfully")
+}
--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module SliceServer
 go 1.25.3
 
 require (
+	filippo.io/edwards25519 v1.2.0 // indirect
+	github.com/go-sql-driver/mysql v1.9.3 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/klauspost/compress v1.17.6 // indirect
 	github.com/thimc/watch v0.0.0-20241103170022-2f2995537b06 // indirect
