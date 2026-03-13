f88912977d3afc5d05058a695418728156b34b7d
Gabriel7721 <116139790+Gabriel7721@users.noreply.github.com>
2026-03-13 16:38:46 +0700

    update order router

--- Routers/OrderRouter.go
+++ Routers/OrderRouter.go
@@ -1,1 +1,30 @@
 package routers
+
+import (
+	controllers "SliceServer/Controllers"
+	"net/http"
+)
+
+func OrderRouter() {
+	http.HandleFunc("/orders", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == "POST" {
+			controllers.CreateOrder(w, r)
+			return
+		}
+		// controllers.CreateNewUser(w, r)
+	})
+
+	// http.HandleFunc("/orders/", func(w http.ResponseWriter, r *http.Request) {
+	// 	switch r.Method {
+	// 	case "GET":
+	// 		controllers.GetUserById(w, r)
+	// 	case "PUT":
+	// 		controllers.UpdateUserById(w, r)
+	// 	case "DELETE":
+	// 		controllers.DeleteUserById(w, r)
+	// 	default:
+	// 		// fmt.Println("Invalid Method")
+	// 		http.Error(w, "Invalid Method", http.StatusMethodNotAllowed)
+	// 	}
+	// })
+}
