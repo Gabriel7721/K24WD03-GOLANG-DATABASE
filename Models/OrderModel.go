8ea5453e1f83487161677d8fa9bcae3046f146fe
Gabriel7721 <116139790+Gabriel7721@users.noreply.github.com>
2026-03-13 16:21:40 +0700

    update order model

--- Models/OrderModel.go
+++ Models/OrderModel.go
@@ -1,1 +1,12 @@
 package models
+
+import "time"
+
+type Order struct {
+	ID          int       `json:"id"`
+	UserID      string    `json:"user_id"`
+	ProductName string    `json:"product_name"`
+	Quantity    int       `json:"quantity"`
+	Price       float64   `json:"price"`
+	CreatedAt   time.Time `json:"created_at"`
+}
