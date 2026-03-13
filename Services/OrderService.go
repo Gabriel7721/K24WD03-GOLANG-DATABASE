81090c269164b1653edb2a403e6e228f2db2a34d
Gabriel7721 <116139790+Gabriel7721@users.noreply.github.com>
2026-03-13 16:29:22 +0700

    UDPATE

--- Services/OrderService.go
+++ Services/OrderService.go
@@ -1,1 +1,15 @@
 package services
+
+import (
+	configs "SliceServer/Configs"
+	models "SliceServer/Models"
+)
+
+func CreateOrder(order models.Order) {
+	configs.MySQLDB.Exec("INSERT INTO orders (user_id,product_name,quantity,price) VALUES (?,?,?,?)")
+}
+func GetAllOrders()     {}
+func GetOrderByID()     {}
+func GetOrderByUserID() {}
+func UpdateOrderByID()  {}
+func DeletedOrderByID() {}
