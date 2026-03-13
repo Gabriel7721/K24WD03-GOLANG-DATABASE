484bf79524d12098188a0c6a3289b30b1346d7d7
Gabriel7721 <116139790+Gabriel7721@users.noreply.github.com>
2026-03-13 16:37:35 +0700

    update controller

--- Controllers/OrderController.go
+++ Controllers/OrderController.go
@@ -1,1 +1,22 @@
 package controllers
+
+import (
+	models "SliceServer/Models"
+	services "SliceServer/Services"
+	"encoding/json"
+	"net/http"
+)
+
+func CreateOrder(w http.ResponseWriter, r *http.Request) {
+	var order models.Order
+	json.NewDecoder(r.Body).Decode(&order)
+
+	if !services.UserExistsById(order.UserID) {
+		http.Error(w, "User Not Found", http.StatusNotFound)
+		return
+	}
+	json.NewEncoder(w).Encode(map[string]any{
+		"Message":  "Order created successfully.",
+		"Order ID": services.CreateOrder(order),
+	})
+}
